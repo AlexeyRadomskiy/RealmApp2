d4ce46c5b074ea3d3c8f61bf87f18842af228146
Alexey <alexeyradomskiy98@gmail.com>
2022-03-28 18:15:37 +0300

    Create Model TaskList

---
+++ RealmApp2/Models/TaskList.swift
@@ -0,0 +1,21 @@
+//
+//  TaskList.swift
+//  RealmApp2
+//
+//  Created by Alexey on 27.03.2022.
+//
+
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
